3a7351a672da6cfc90ff0ce02a7ab0acd29250fe
Leonardo Mopaca <leonardomopaca@gmail.com>
2024-02-22 15:43:05 -0600

    Test

--- math.go
+++ math.go
@@ -4,8 +4,11 @@ import "fmt"
 
 func main() {
 	fmt.Println(soma(112, 10))
+	fmt.Println(mult(2, 4))
 }
 
 func soma(a int, b int) int {
 	return a + b
 }
+
+func mult(a int, b int) int { return a * b }
