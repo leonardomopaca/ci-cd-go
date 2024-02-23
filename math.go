c453fc8b74140b914d6c7512a61d640c3254b1f1
Leonardo Mopaca <leonardomopaca@gmail.com>
2024-02-22 20:56:12 -0600

    Modifying to branch develop as default

--- math.go
+++ math.go
@@ -4,11 +4,11 @@ import "fmt"
 
 func main() {
 	fmt.Println(soma(112, 10))
-	fmt.Println(multiplica(2, 4))
+	fmt.Println(multip(2, 4))
 }
 
 func soma(a int, b int) int {
 	return a + b
 }
 
-func multiplica(a float64, b float64) float64 { return a * b }
+func multip(a float64, b float64) float64 { return a * b }
